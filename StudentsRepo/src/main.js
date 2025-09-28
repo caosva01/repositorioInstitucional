0171f756a872990e3be7fd494726b329bd86a06f
vcorreaga <viadis085@hotmail.com>
2025-09-28 09:54:47 -0500

    Organización de estructura: se agregó el código del main (cerebro central para detectar páginas), se reorganizó la estructura del proyecto y se actualizó el README con más detalles

---
+++ StudentsRepo/src/main.js
@@ -0,0 +1,34 @@
+
+import { initLogin } from "./scripts/login.js";
+import { initAdmin } from "./scripts/admin.js";
+import { initProyectos } from "./scripts/proyecto.js";
+import { initDetalleProyecto } from "./scripts/detalleProyecto.js";
+import { initRecover } from "./scripts/recover.js";
+
+
+const ruta = window.location.pathname;
+
+
+if (ruta.includes("login.html")) {
+  initLogin();
+}
+
+if (ruta.includes("admin.html")) {
+  initAdmin();
+}
+
+if (ruta.includes("proyecto.html")) {
+  initProyectos();
+}
+
+if (ruta.includes("detalle-proyecto.html")) {
+  initDetalleProyecto();
+}
+
+if (ruta.includes("recover.html")) {
+  initRecover();
+}
+
+if (ruta.includes("index.html")) {
+  console.log("Bienvenido a StudentsRepo");
+}
